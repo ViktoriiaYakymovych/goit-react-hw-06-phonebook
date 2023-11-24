81df5866564225b10ab6b28955e1926a7e21735b
ViktoriiaYakymovych <118008244+ViktoriiaYakymovych@users.noreply.github.com>
2023-11-24 19:16:56 +0200

    a

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,4 +1,3 @@
-// import { useDispatch } from 'react-redux';
 import { Formik } from 'formik';
 import { nanoid } from 'nanoid';
 import * as Yup from 'yup';
@@ -19,7 +18,6 @@ const schema = Yup.object().shape({
 });
 
 export const ContactForm = ({ addContact }) => {
-  // const dispatch = useDispatch();
   return (
     <Formik
       initialValues={{ name: '', number: '' }}
