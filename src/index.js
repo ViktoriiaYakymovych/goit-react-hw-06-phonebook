8b864bd39cdf95feafc523fe81f9e1f767b6d22c
ViktoriiaYakymovych <118008244+ViktoriiaYakymovych@users.noreply.github.com>
2023-11-30 11:45:00 +0200

    a

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store } from 'redux/store';
+// import { PersistGate } from 'redux-persist/integration/react';
+import {
+  store,
+  // persistior
+} from 'redux/store';
 import { App } from 'components/App/App';
 import GlobalStyle from 'components/GlobalStyle';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
+      {/* <PersistGate loading={null} persistor={persistior}> */}
       <App />
       <GlobalStyle />
+      {/* </PersistGate> */}
     </Provider>
   </React.StrictMode>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -6,3 +6,23 @@ import { filterReducer } from './filterSlice';
 export const store = configureStore({
   reducer: { contacts: contactsReducer, filter: filterReducer },
 });
+
+// import { configureStore } from '@reduxjs/toolkit';
+// import { persistStore, persistReducer } from 'redux-persist';
+// import storage from 'redux-persist/lib/storage';
+
+// import { contactsReducer } from './contactsSlice';
+// import { filterReducer } from './filterSlice';
+
+// const persistConfig = {
+//   key: 'root',
+//   storage,
+// };
+
+// const persistedcontactsReducer = persistReducer(persistConfig, contactsReducer);
+
+// export const store = configureStore({
+//   reducer: { contacts: persistedcontactsReducer, filter: filterReducer },
+// });
+
+// export const persistior = persistStore(store);
